e5be833d8c25329090b1f71d3496ffc0e7130705
ndelamaide <nicolas.delamaide@gmail.com>
2021-11-03 19:11:11 +0100

    perfectlink with comments

--- template_cpp/src/src/perfectlink.cpp
+++ template_cpp/src/src/perfectlink.cpp
@@ -86,7 +86,7 @@ void Perfectlink::sendThreaded() {
                 }
             }
 
-            //std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
         }
     }
 }
